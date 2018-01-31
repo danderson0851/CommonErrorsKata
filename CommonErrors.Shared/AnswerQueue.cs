3da4682f70dfc5284b549e6cf83310791cd4babd
danderson0851 <danderson0851@gmail.com>
2018-01-31 10:15:08 -0600

    Refactored TrueFalseAnswer.cs & deleted empty line spaces

--- CommonErrors.Shared/AnswerQueue.cs
+++ CommonErrors.Shared/AnswerQueue.cs
@@ -7,16 +7,13 @@ namespace CommonErrors.Shared
     public class AnswerQueue<T> : Queue<T> where T : IGradable
     {
         private readonly int _size;
-
         public decimal Grade { get{ return !this.Any() ? 0 :  this.Average(x => x.Grade); } }
-
         /// <inheritdoc />
         /// <summary>
         /// Stack that cannot exceed it's size
         /// </summary>
         /// <param name="size">Maximum size of the queue</param>
         public AnswerQueue(int size) => _size = size;
-
         /// <summary>
         /// Hides the default implementation of queue Enqueue 
         /// </summary>
@@ -27,11 +24,7 @@ namespace CommonErrors.Shared
             {
                 Dequeue();
             }
-
             base.Enqueue(item);
         }
-
-        
-        
     }
 }
